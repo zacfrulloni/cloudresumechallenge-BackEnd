7b1e81166aa1643929c11d179a5aa994af8ea97d
zacfrulloni <56273069+zacfrulloni@users.noreply.github.com>
2020-12-11 21:09:43 +0000

    Add files via upload

---
+++ Test_app.py
@@ -0,0 +1,17 @@
+import boto3
+import json
+dynamodb = boto3.resource('dynamodb', region_name="eu-west-2")
+table= dynamodb.Table('zacresumetable2')
+
+def lambda_handler(event, context):
+    response= table.update_item(
+    Key= {'URL': 'zacresume.com'},
+    UpdateExpression= "SET visits = visits + :increase",
+    ExpressionAttributeValues= {':increase': 1},
+    ReturnValues= "UPDATED_NEW"
+)
+    return {'statusCode': 200,
+            'body': json.dumps('visitsUpdated'),
+            'headers': {'Content-Type': 'application/json'}}
+print("UPDATING ITEM")
+print("response") 
