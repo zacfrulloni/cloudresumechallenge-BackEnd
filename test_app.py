5a62743e7db887516affee337c09ec1d9edc4485
zacfrulloni <56273069+zacfrulloni@users.noreply.github.com>
2020-12-12 11:31:37 +0000

    Add files via upload

--- test_app.py
+++ test_app.py
@@ -1,8 +1,7 @@
 import boto3
 import json
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.resource('dynamodb',region_name= 'eu-west-2')
 table= dynamodb.Table('zacresumetable2')
-region_name= 'eu-west-2'
 
 def lambda_handler(event, context):
     response= table.update_item(
