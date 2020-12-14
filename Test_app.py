d64216466c5a012e1b554796c11491f341e0beef
zacfrulloni <56273069+zacfrulloni@users.noreply.github.com>
2020-12-14 14:33:45 +0000

    Update Test_app.py

--- Test_app.py
+++ Test_app.py
@@ -1,7 +1,8 @@
 import boto3
 import json
-dynamodb = boto3.resource('dynamodb', region_name= 'eu-west-2')
+dynamodb = boto3.resource('dynamodb')
 table= dynamodb.Table('zacresumetable2')
+setx AWS_DEFAULT_REGION= 'eu-west-2'
 
 def lambda_handler(event, context):
     response= table.update_item(
