26cb33711ae1909c406788d0205a6ddae9e3aba2
zacfrulloni <56273069+zacfrulloni@users.noreply.github.com>
2020-12-15 16:18:02 +0000

    Update Test_app.py

--- Test_app.py
+++ Test_app.py
@@ -4,9 +4,9 @@ import json
 dynamodb = boto3.resource('dynamodb', region_name= 'eu-west-2')
 table= dynamodb.Table('zacresumetable2')
 
-def test_event(dyanmodb):
+def test_event():
     assert True
-def test_context(dyanmodb):
+def test_context():
     assert 1 == 1
 def lambda_handler(event, context):
     response= table.update_item(
