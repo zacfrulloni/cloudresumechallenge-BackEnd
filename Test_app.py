a5b3c09fca14ffce6e1230e1d47aef20b0100b94
zacfrulloni <56273069+zacfrulloni@users.noreply.github.com>
2020-12-15 13:20:51 +0000

    Update Test_app.py

--- Test_app.py
+++ Test_app.py
@@ -4,7 +4,7 @@ import json
 dynamodb = boto3.resource('dynamodb', region_name= 'eu-west-2')
 table= dynamodb.Table('zacresumetable2')
 
-def pytest_lambda_handler(event, context):
+def test_lambda_handler(event, context):
     response= table.update_item(
     Key= {'URL': 'zacresume.com'},
     UpdateExpression= "SET visits = visits + :increase",
