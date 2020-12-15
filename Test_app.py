36a415dded874cd1dc5bc9831435472a8360a4b8
zacfrulloni <56273069+zacfrulloni@users.noreply.github.com>
2020-12-15 10:46:56 +0000

    Update Test_app.py

--- Test_app.py
+++ Test_app.py
@@ -1,7 +1,6 @@
 import boto3
-region_name= 'eu-west-2'
 import json
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.resource('dynamodb', region_name= 'eu-west-2')
 table= dynamodb.Table('zacresumetable2')
 
 def lambda_handler(event, context):
